03f3fe41533b2aad0b5a9b846dc3962ed1bf3a64
Hiago 7k <hiago7k@gmail.com>
2026-08-03 18:19:18 -0300

    estrutura inicial

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 class Main{
     static void main() {
         System.out.println("Try e catch");
+        Scanner teclado = new Scanner(system.in);
+        System.out.println("Digite um número: ");
+        double primeiroNumero = teclado.nextDouble();
+
+        System.out.println("Digite outro número: ");
+        double segundoNumero = teclado.nextDouble();
+        
+
     }
 }
