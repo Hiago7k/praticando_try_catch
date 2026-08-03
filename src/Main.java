36ac1eafe79916b5c9b695dce82e7a3d2530b67a
Hiago 7k <hiago7k@gmail.com>
2026-08-03 18:20:27 -0300

    fix scanner

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,16 @@ import java.util.Scanner;
 class Main{
     static void main() {
         System.out.println("Try e catch");
-        Scanner teclado = new Scanner(system.in);
+        Scanner  teclado = new Scanner(System.in);
         System.out.println("Digite um número: ");
         double primeiroNumero = teclado.nextDouble();
 
         System.out.println("Digite outro número: ");
         double segundoNumero = teclado.nextDouble();
-        
+
+
+        System.out.println(primeiroNumero);
+        System.out.println(segundoNumero);
 
     }
 }
