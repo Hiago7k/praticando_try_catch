24f36629bcfcb27b2c2a143b0d7306f5bd801a46
Hiago 7k <hiago7k@gmail.com>
2026-08-03 18:36:59 -0300

    novo desafio

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,8 @@ import java.util.Scanner;
 
 class Main{
     static void main() {
-
-        System.out.println("Try e catch");
-        Scanner  teclado = new Scanner(System.in);
-        System.out.println("Digite um número: ");
-        int  primeiroNumero = teclado.nextInt();
-        System.out.println("Digite outro número: ");
-        int segundoNumero = teclado.nextInt();
-
-        try{
-
-        int resultado = primeiroNumero / segundoNumero;
-            System.out.println("Resultado da div" + resultado);
-        }catch (ArithmeticException  e)
-        {
-            System.out.println("Erro");
-        }
+        System.out.println("***************");
+        System.out.println("Strong Password");
+        System.out.println("***************");
     }
 }
